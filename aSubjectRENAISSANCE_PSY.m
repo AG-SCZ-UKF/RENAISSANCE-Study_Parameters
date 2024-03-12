3986824c572cfd62027e5e28181f740b3445c9fb
AG-SCZ-UKF <104207083+AG-SCZ-UKF@users.noreply.github.com>
2024-03-12 12:10:27 +0100

    Update first subject

--- aSubjectRENAISSANCE_PSY.m
+++ aSubjectRENAISSANCE_PSY.m
@@ -1,12 +1,11 @@
 function aSubject = aSubjectRENAISSANCE_PSY()
 
 aSubject.RENAISSANCE_PSY.Groups.CONT	= {
-	'RZG391QU'
-	'XHM715FA'
 	};
 aSubject.RENAISSANCE_PSY.Groups.CONT = sort(aSubject.RENAISSANCE_PSY.Groups.CONT);
 
 aSubject.RENAISSANCE_PSY.Groups.SCHI	= {
+	'TRV397PN'
 	};
 aSubject.RENAISSANCE_PSY.Groups.SCHI = sort(aSubject.RENAISSANCE_PSY.Groups.SCHI);
 
